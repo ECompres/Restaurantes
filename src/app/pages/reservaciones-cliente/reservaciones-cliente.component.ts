365a69fe6bc18f4ca8fe0f7b9f64eecc9f7959bd
Nayeli Gonzalez <nayi_n13@hotmail.com>
2020-09-08 20:32:52 -0400

    paginador

--- src/app/pages/reservaciones-cliente/reservaciones-cliente.component.ts
+++ src/app/pages/reservaciones-cliente/reservaciones-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-reservaciones-cliente',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservacionesClienteComponent implements OnInit {
 
-  nombreCliente = 'Elías';
-
-  reservation = [
+  nombreCliente = 'Elías'; 
+  page = 1;
+  returnedArray: Array<any>;
+ 
+  contentArray =[
     {
       restaurant: 'Papitas calientes',
       state: 'Pensilvania',
@@ -29,12 +32,47 @@ export class ReservacionesClienteComponent implements OnInit {
       restaurant: 'Papitas calientes',
       state: 'Pensilvania',
       city: 'Philadelphia'
+    },    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
     },
+    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
+    },
+    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
+    },
+    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
+    },
+    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
+    },    {
+      restaurant: 'Papitas calientes',
+      state: 'Pensilvania',
+      city: 'Philadelphia'
+    }
+   
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.returnedArray = this.contentArray.slice(0, 1);
   }
-
+ 
+  pageChanged(event: PageChangedEvent): void {
+    const startItem = (event.page - 1) * event.itemsPerPage;
+    const endItem = event.page * event.itemsPerPage;
+    this.returnedArray = this.contentArray.slice(startItem, endItem);
+  }
 }
