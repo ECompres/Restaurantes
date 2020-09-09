a5072dbdb701d8710716350b19f7cbde9d01d0bf
Elias Compres <ecompres2017@gmail.com>
2020-09-08 21:49:21 -0400

    Get Restaurants in Client reservations

--- src/app/pages/reservaciones-cliente/reservaciones-cliente.component.ts
+++ src/app/pages/reservaciones-cliente/reservaciones-cliente.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { UserService } from '../../Services/user.service';
+import { User } from '../../Models/user';
+import { Restaurants } from 'src/app/Models/restaurants';
+import { RestaurantService } from 'src/app/Services/restaurant.service';
 
 @Component({
   selector: 'app-reservaciones-cliente',
@@ -12,6 +16,10 @@ export class ReservacionesClienteComponent implements OnInit {
   nombreCliente: string = localStorage.getItem("name") + " " + localStorage.getItem("lastName");
   page = 1;
   returnedArray: Array<any>;
+  userID: string = localStorage.getItem("idUser")
+  usuario: User;
+
+  restaurantesCliente = [];
 
   contentArray = [
     {
@@ -65,10 +73,42 @@ export class ReservacionesClienteComponent implements OnInit {
 
   ];
 
-  constructor() { }
+  constructor(private userService: UserService, private restaurantService: RestaurantService) { }
+
+  loadRestaurants(id) {
+    this.restaurantService.getRestaurant(id).subscribe(
+      (res) => {
+        this.restaurantesCliente.push(res);
+        console.log(this.restaurantesCliente);
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
 
   ngOnInit(): void {
     this.returnedArray = this.contentArray.slice(0, 1);
+    if (this.userID != null) {
+      this.userService.getUsuario(this.userID).subscribe(
+        (res) => {
+          this.usuario = {
+            name: res.data().name,
+            lastName: res.data().lastName,
+            email: res.data().email,
+            password: res.data().password,
+            reservations: res.data().reservations
+          };
+          if (this.usuario.reservations.length > 0) {
+            for (let i = 0; i < this.usuario.reservations.length; i++) {
+              this.loadRestaurants(this.usuario.reservations[i].idRestaurant)
+            }
+          }
+        },
+        (err) => console.log(err)
+      );
+    }
+
   }
 
   pageChanged(event: PageChangedEvent): void {
